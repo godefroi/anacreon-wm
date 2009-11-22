87322a154474c6a43d6b0ceeda45dae7115dae7e
godefroi <godefroi@6ea7bc3e-d453-11de-b7d7-53e004a27ad6>
2009-11-22 14:53:01 +0000

    WorldForm implemented, new DialogForm base and ItemControl for use in multiple places

---
+++ trunk/Anacreon.Mobile/DialogForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Anacreon.Mobile
+{
+	public partial class DialogForm : Form
+	{
+		public DialogForm()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnPaintBackground(PaintEventArgs e)
+		{
+			base.OnPaintBackground(e);
+
+			var line_w = 3;
+			var l_x    = 7;
+			var r_x    = ClientRectangle.Width - 10;
+			var t_y    = 7;
+			var b_y    = ClientRectangle.Height - 10;
+			var v_h    = b_y - t_y;
+			var h_w    = r_x - l_x + line_w;
+
+			// left line
+			e.Graphics.FillRectangle(Brushes.Gray, l_x, t_y, line_w, v_h);
+
+			// right line
+			e.Graphics.FillRectangle(Brushes.Gray, r_x, t_y, line_w, v_h);
+
+			// bottom line
+			e.Graphics.FillRectangle(Brushes.Gray, l_x, b_y, h_w, line_w);
+		}
+
+		protected override void OnResize(EventArgs e)
+		{
+			base.OnResize(e);
+
+			DialogPanel.Top    = 16;
+			DialogPanel.Left   = 16;
+			DialogPanel.Width  = ClientRectangle.Width - 32;
+			DialogPanel.Height = ClientRectangle.Height - 32;
+		}
+	}
+}
---
+++ trunk/Anacreon.Mobile/ItemControl.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Anacreon.Mobile
+{
+	partial class ItemControl
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if( disposing && (components != null) )
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.LabelLabel = new System.Windows.Forms.Label();
+			this.ValueLabel = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// LabelLabel
+			// 
+			this.LabelLabel.Dock = System.Windows.Forms.DockStyle.Top;
+			this.LabelLabel.Font = new System.Drawing.Font("Courier New", 8F, System.Drawing.FontStyle.Bold);
+			this.LabelLabel.ForeColor = System.Drawing.Color.LightGray;
+			this.LabelLabel.Location = new System.Drawing.Point(0, 0);
+			this.LabelLabel.Name = "LabelLabel";
+			this.LabelLabel.Size = new System.Drawing.Size(150, 24);
+			this.LabelLabel.Text = "lbl";
+			this.LabelLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			// 
+			// ValueLabel
+			// 
+			this.ValueLabel.BackColor = System.Drawing.Color.DarkBlue;
+			this.ValueLabel.Dock = System.Windows.Forms.DockStyle.Top;
+			this.ValueLabel.Font = new System.Drawing.Font("Courier New", 8F, System.Drawing.FontStyle.Bold);
+			this.ValueLabel.ForeColor = System.Drawing.Color.LightGray;
+			this.ValueLabel.Location = new System.Drawing.Point(0, 24);
+			this.ValueLabel.Name = "ValueLabel";
+			this.ValueLabel.Size = new System.Drawing.Size(150, 24);
+			this.ValueLabel.Text = "val";
+			this.ValueLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			// 
+			// ItemControl
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			this.BackColor = System.Drawing.Color.DarkBlue;
+			this.Controls.Add(this.ValueLabel);
+			this.Controls.Add(this.LabelLabel);
+			this.Name = "ItemControl";
+			this.Size = new System.Drawing.Size(150, 48);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label LabelLabel;
+		private System.Windows.Forms.Label ValueLabel;
+	}
+}
